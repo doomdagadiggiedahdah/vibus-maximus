aef30e8bdf7c1ae867f67c4480ef8b2fc7007f9c
mat <matdallen@gmail.com>
2025-03-08 16:44:05 -0800

    WORKS

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'src/obsidian-plugin/main.ts',
   output: {
-    file: 'main.js',
+    file: 'test-notes/.obsidian/plugins/vibe-boi/main.js',
     sourcemap: 'inline',
     format: 'cjs',
     exports: 'default',
